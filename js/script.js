1c630119183cda698ab599ff45e043be06f86f2a
BetterCallAle <dap22091998@gmail.com>
2022-10-07 15:31:01 +0200

    extracted HTML elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-//estraggo i dati dall'html
-const distance = document.getElementById("km")
+//i dati
+const basePrice = 0.21;
+const saleUnder = 0.20;
+const saleOver = 0.40;
+
+//html extracted elements
+const userDistance = document.getElementById("km");
+console.log(userDistance);
+
+const userAge = document.getElementById("age");
+console.log(userAge);
+
+const submit = document.getElementById("submit");
+console.log(submit);
+
+submit.addEventListener("click" function() {
+    const userDistanceValue = userDistance.ariaValueMax;
+})
